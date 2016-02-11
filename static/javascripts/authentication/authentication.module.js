908a36aed21467124229230a3ab93165b583d225
Javier Adriel <reivaj0705@gmail.com>
2016-02-11 16:13:56 -0600

    Add authentication module

---
+++ static/javascripts/authentication/authentication.module.js
@@ -0,0 +1,8 @@
+(function  () {
+    "use strict";
+    angular
+        .module("thinkster.authentication", [
+            "thinkster.authentication.controllers",
+            "thinkster.authentication.services"   
+        ]);
+})
