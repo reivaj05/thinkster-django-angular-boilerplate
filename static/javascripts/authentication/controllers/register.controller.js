b5e0469ecc42871881c5b6b68edd29b1b3e19f25
Javier Adriel <reivaj0705@gmail.com>
2016-02-11 15:54:41 -0600

    Add RegisterController

---
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -0,0 +1,17 @@
+(function() {
+    "use strict";
+
+    angular
+        .module("thinkster.authentication.controllers", [])
+        .controller("RegisterController", RegisterController);
+
+        RegisterController.$inject = ["$location", "$scope", "Authentication"];
+
+        function RegisterController($location, $scope, Authentication){
+            var self = this;
+
+            function register () {
+                Authentication.register(self.email, self.password, self.username);
+            }
+        }
+})();
