d04466e7712d757118997d49259d74b4da738027
Javier Adriel <reivaj0705@gmail.com>
2016-02-12 10:57:18 -0600

    Add CSRF settings

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -5,5 +5,13 @@
         .module("thinkster", [
             "thinkster.routes",
             "thinkster.authentication"
-        ]);
+        ])
+        .run(run);
+
+        run.$inject = ["$http"];
+
+        function run ($http) {
+            $http.defaults.xsrfHeaderName = "X-CSRFToken";
+            $http.defaults.xsrfCookieName = "csrftoken";
+        }
 })();
