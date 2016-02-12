8acce0dbe90985ecc3b5ef4b97d17e327028ab84
Javier Adriel <reivaj0705@gmail.com>
2016-02-12 12:36:14 -0600

    Redirect authenticated users to the index view in RegisterController

--- static/javascripts/authentication/controllers/register.controller.js
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -15,5 +15,19 @@
             function register () {
                 Authentication.register(self.email, self.password, self.username);
             }
+
+            activate();
+
+            /**
+             * @name activate
+             * @desc Actions to be performed when this controller is instantiated
+             * @memberOf thinkster.authentication.controllers.RegisterController
+             */
+            function activate() {
+              // If the user is authenticated, they should not be here.
+              if (Authentication.isAuthenticated()) {
+                $location.url('/');
+              }
+            }
         }
 })();
