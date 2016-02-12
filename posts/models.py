0ff65485fca18926c8dbcd3d5ac402e283f3b076
Javier Adriel <reivaj0705@gmail.com>
2016-02-12 13:32:36 -0600

    Add Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from authentication.models import Account
+
 
 # Create your models here.
+class Post(models.Model):
+
+    author = models.ForeignKey(Account)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __unicode__(self):
+        return '{0}'.format(self.content)
