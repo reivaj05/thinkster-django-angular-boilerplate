462638a4e580aa222a6eb94db9d64f6528e6b7c2
Javier Adriel <reivaj0705@gmail.com>
2016-02-12 11:18:45 -0600

    Fix typo and add imports

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,5 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, status, views, viewsets
+from rest_framework.response import Response
 from .models import Account
 from .permissions import IsAccountOwner
 from .serializers import AccountSerializer
@@ -10,7 +11,7 @@ class AccountViewSet(viewsets.ModelViewSet):
     serializer_class = AccountSerializer
 
     def get_permissions(self):
-        if self.resquest.method in permissions.SAFE_METHODS:
+        if self.request.method in permissions.SAFE_METHODS:
             return (permissions.AllowAny(), )
 
         if self.request.method == 'POST':
@@ -19,8 +20,9 @@ class AccountViewSet(viewsets.ModelViewSet):
         return (permissions.IsAuthenticated(), IsAccountOwner(), )
 
     def create(self, request):
+        print request.data
         serializer = self.serializer_class(data=request.data)
-
+        print serializer.is_valid()
         if serializer.is_valid():
             Account.objects.create_user(**serializer.validated_data)
 
