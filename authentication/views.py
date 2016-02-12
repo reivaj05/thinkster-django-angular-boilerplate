ea83abfc3ed540773529b0f0334ef20232cf6cd6
Javier Adriel <reivaj0705@gmail.com>
2016-02-12 11:28:29 -0600

    Add login view

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,36 @@
+import json
+from django.contrib.auth import authenticate, login
 from rest_framework import permissions, status, views, viewsets
 from rest_framework.response import Response
 from .models import Account
 from .permissions import IsAccountOwner
 from .serializers import AccountSerializer
+
+
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+
+        if account:
+            if account.is_active():
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+                return Response(serialized.data)
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username/password combination invalid'
+            }, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class AccountViewSet(viewsets.ModelViewSet):
