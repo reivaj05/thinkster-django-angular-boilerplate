31ee3522d3ddf5969ef885bc80c15be4cf7e589f
Javier Adriel <reivaj0705@gmail.com>
2016-02-13 17:42:41 -0600

    Add route for updating user

--- static/javascripts/thinkster.routes.js
+++ static/javascripts/thinkster.routes.js
@@ -22,6 +22,11 @@
                 controllerAs: "vm",
                 templateUrl: "/static/templates/profiles/profile.html"
             })
+            when("/+:username/settings", {
+                controller: "ProfileSettingsController",
+                controllerAs: "vm",
+                templateUrl: "/static/templates/profiles/settings.html"
+            })
             .when("/", {
                 controller: "IndexController",
                 controllerAs: "vm",
